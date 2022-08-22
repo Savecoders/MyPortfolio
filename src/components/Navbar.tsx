d9f8c5e05aee5cc7b6cdadefb28197a45510ecb3
Savecoders <pablopincay2003@gmail.com>
2022-08-21 23:07:42 -0500

    init project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <div className='bg-bg p-6'>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/*@type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      screen: {
+        tablet: '960px',
+        desktop: '1280px',
+      }
+    },
+    colors: {
+      bg: '#131417',
+      sec: '#17191F',
+      ter: '#14131D',
+      icons: '#FEFEFE',
+      primary: '#E7ECEF',
+      title: '#E1ECF7',
+      title_sec: '#EBF0FF',
+      blue: '#6277D7',
+      cyan: '#4773BA',
+    },
+  },
+  plugins: [],
+};
